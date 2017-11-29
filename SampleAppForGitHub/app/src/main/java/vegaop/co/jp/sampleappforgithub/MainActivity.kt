17bdb3996f302597a12ce3f265ec5c320eaf9d52
v-miyazaki <miyazaki@vega-op.co.jp>
2017-11-29 20:39:31 +0900

    サンプルプロジェクトを追加

---
+++ SampleAppForGitHub/app/src/main/java/vegaop/co/jp/sampleappforgithub/MainActivity.kt
@@ -0,0 +1,12 @@
+package vegaop.co.jp.sampleappforgithub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
